22fb23b018ef1e46662b953a2cffa899282072eb
AbhishekGowda <abhi.gowda28@gmail.com>
2018-06-08 23:09:48 +0530

    sample code ran

---
+++ learning/chapter-1/app.js
@@ -0,0 +1,12 @@
+class ProductList extends React.Component{
+    render(){
+        return(
+            <div>
+                <p> Hello friend!! I am learning React for web development</p>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<ProductList />, document.getElementById("content"));
