d1cdbf0fae698065d918e5c47e2f319414ed8543
AbhishekGowda <abhi.gowda28@gmail.com>
2018-06-10 11:15:26 +0530

    hard coded ipl team pooling

--- learning/chapter-1/app.js
+++ learning/chapter-1/app.js
@@ -1,12 +1,41 @@
-class ProductList extends React.Component{
+class Team extends React.Component{
+
+    render(){
+        return(<div className="team">
+            <div className="image">
+                <img src="./images/rcb.jpg" alt="RCB"/>
+            </div>
+            <div className="votes">
+                <div className="up"></div>
+                <div className="total">59</div>
+                <div className="down"></div>
+            </div>
+            <div className="teamDetails">
+                <div className="shortName">RCB</div>
+                <div className="name">Royal Challengers Bangalore</div>
+                <div className="description">Team representing Bengaluru, lead by Virat Kohli"</div>
+                <div className="slogon">Play Bold</div>
+            </div>
+        </div>);
+    }
+}
+
+
+
+class TeamList extends React.Component{
     render(){
         return(
             <div>
-                <p> Hello friend!! I am learning React for web development</p>
+                <Team/>
+                <Team/>
+                <Team/>
+                <Team/>
+                <Team/>
+                <Team/>
             </div>
         );
     }
 }
 
 
-ReactDOM.render(<ProductList />, document.getElementById("content"));
+ReactDOM.render(<TeamList />, document.getElementById("content"));
