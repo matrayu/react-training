4fcb76dce6e3408c3ee737c859896b9a3878c4e7
AbhishekGowda <abhi.gowda28@gmail.com>
2018-06-11 22:54:43 +0530

    Added states and lifecycle to the component

--- learning/chapter-1/app.js
+++ learning/chapter-1/app.js
@@ -1,14 +1,31 @@
 class Team extends React.Component{
 
+    constructor(props){
+        super(props);
+
+        this.handleDownVote = this.handleDownVote.bind(this);
+        this.handleUpVote = this.handleUpVote.bind(this);
+    }
+
+
+    handleUpVote(){
+        this.props.onUpVote(this.props.teamID);
+    }
+
+    handleDownVote(){
+        this.props.onUpVote(this.props.teamID);
+    }
+
+
     render(){
-        return(<div className="team">
+        return (<div className="team">
             <div className="image">
                 <img src={this.props.img} alt={this.props.team}/>
             </div>
             <div className="votes">
-                <div className="up"></div>
+                <div className="up" onClick={this.handleUpVote}></div>
                 <div className="total">{this.props.votes}</div>
-                <div className="down"></div>
+                <div className="down" onClick={this.handleDownVote}></div>
             </div>
             <div className="teamDetails">
                 <div className="shortName">{this.props.team}</div>
@@ -23,11 +40,27 @@ class Team extends React.Component{
 
 
 class TeamList extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            iplteams : []
+        }
+    }
+
+
+    componentDidMount(){
+        this.setState({
+            iplteams : Seed.iplteams
+        });
+    }
+
     render(){
 
         // fetching the teams data and converting them into react representable data
-        const teams = Seed.iplteams.map((team)=>{
-            return <Team key={team.id} name={team.name} team={team.team} description={team.description} slogon={team.slogon} img={team.image} votes={team.votes}/>
+        const teams = this.state.iplteams.map((team)=>{
+            return <Team key={team.id} teamID={'team '+team.id} name={team.name} team={team.team} description={team.description} slogon={team.slogon} img={team.image} votes={team.votes} onUpVote={this.handleTeamUpVote} onDownVote={this.handleTeamDownVote}/>
         });
 
 
@@ -37,6 +70,17 @@ class TeamList extends React.Component{
 
         return (<div>{teams}</div>);
     }
+
+    handleTeamUpVote(teamID){
+        console.log("Team ID is ==> ",teamID);
+    }
+
+
+    handleTeamDownVote(teamID) {
+        console.log("Team ID is ==> ", teamID);
+    }
+
+
 }
 
 
